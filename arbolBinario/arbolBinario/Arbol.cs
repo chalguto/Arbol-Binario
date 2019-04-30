d2331b191e968580e5f94d116d905bd58017fb8c
chalguto <chalguto@hotmail.com>
2019-04-29 23:22:29 -0400

    init

---
+++ arbolBinario/arbolBinario/Arbol.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _arbolBinario
+{
+
+
+    public class ArbolBinario
+    {
+
+        private Nodo raiz;
+        int cant;
+
+        public ArbolBinario()
+        {
+            this.raiz = null;
+        }
+
+        public void agregar(int dato)
+        {
+            Nodo nuevo = new Nodo(dato, null, null);
+            insertar(nuevo, raiz);
+        }
+
+        public void insertar(Nodo nuevo, Nodo pivote)
+        {
+            if (this.raiz == null)
+            {
+                raiz = nuevo;
+            }
+            else
+            {
+                if (nuevo.getDato() <= pivote.getDato())
+                {
+                    if (pivote.getIzq() == null)
+                    {
+                        pivote.setIzq(nuevo);
+                    }
+                    else
+                    {
+                        insertar(nuevo, pivote.getIzq());
+                    }
+                }
+                else
+                {
+                    if (pivote.getDer() == null)
+                    {
+                        pivote.setDer(nuevo);
+                    }
+                    else
+                    {
+                        insertar(nuevo, pivote.getDer());
+                    }
+                }
+            }
+
+        }
+
+        //metodos nuevos
+        public bool existe(int info)
+        {
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                cant++;
+                if (info == reco.getDato()) return true;
+                else if (info > reco.getDato()) reco = reco.getDer();
+                else reco = reco.getIzq();
+            }
+            return false;
+        }
+
+        public int cantidad()
+        {
+            cant = 0;
+            cantidad(raiz);
+            return cant;
+        }
+
+        private void cantidad(Nodo reco)
+        {
+            if (reco != null)
+            {
+                cant++;
+                cantidad(reco.getIzq());
+                cantidad(reco.getDer());
+            }
+        }
+
+        private void cantidadNodosHoja(Nodo reco)
+        {
+            if (reco != null)
+            {
+                if (reco.getIzq() == null && reco.getDer() == null)
+                {
+                    cant++;
+                }
+                cantidadNodosHoja(reco.getIzq());
+                cantidadNodosHoja(reco.getDer());
+            }
+        }
+
+        public int cantidadNodosHoja()
+        {
+            cant = 0;
+            cantidadNodosHoja(raiz);
+            return cant;
+        }
+
+        public int encontrarNodos(int inf)
+        {
+            int encont = 0;
+            cant = 0;
+            if (existe(inf)) encont = cant;
+            return encont > 0 ? encont - 1 : encont;
+        }
+
+    }
+
+}
+
+
+
---
+++ arbolBinario/arbolBinario/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _arbolBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinario abo = new ArbolBinario();
+            int dt;
+
+            Console.WriteLine(" Insertando los siguientes 7 valores ");
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.Write($"Inserte valor {i}: ");
+                abo.agregar(Convert.ToInt32(Console.ReadLine().ToString()));
+            }
+
+            do
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.WriteLine("1.- Cantidad de nodos del arbol");
+                Console.WriteLine("2.- Cantidad de nodos hoja");
+                Console.WriteLine("3.- Buscar la distancia entre la raiz y un nodo");
+                Console.WriteLine("0.- Salir");
+                dt = Convert.ToInt32(Console.ReadLine().ToString());
+
+                switch (dt)
+                {
+                    case 1:
+                        Console.WriteLine("Cantidad de nodos del árbol:" + abo.cantidad());
+                        break;
+                    case 2:
+                        Console.WriteLine("Cantidad de nodos hoja:" + abo.cantidadNodosHoja());
+                        break;
+                    case 3:
+                        Console.Write("Ingrese nodo: ");
+                        int nodo = Convert.ToInt32(Console.ReadLine().ToString());
+
+                        Console.WriteLine("Distancia entre la raíz y el nodo: " + abo.encontrarNodos(nodo));
+                        break;
+                }
+            } while (dt != 0);
+
+
+            Console.Read();
+        }
+    }
+
+}
